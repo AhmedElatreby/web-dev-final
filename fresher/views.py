640f9cead609510380eae716441a58377e75f8b4
ahmed <ahmedsport@hotmail.com>
2021-06-05 22:20:01 +0100

    :art: working of basket :art:

--- fresher/views.py
+++ fresher/views.py
@@ -5,15 +5,15 @@ from .models import Category, Recipe
 
 def all_recipes(request):
     recipes = Recipe.objects.all()
-    return render(request, 'home.html', {'recipes': recipes})
+    return render(request, 'fresher/home.html', {'recipes': recipes})
 
 
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
     recipes = Recipe.objects.filter(category=category)
-    return render(request, "fresher/category.html", {"recipes": recipes})
+    return render(request, "fresher/recipe/category.html", {"recipes": recipes})
 
 
 def recipe_detail(request, slug):
     recipe = get_object_or_404(Recipe, slug=slug)
-    return render(request, 'fresher/single.html', {'recipe': recipe})
+    return render(request, 'fresher/recipe/single.html', {'recipe': recipe})
